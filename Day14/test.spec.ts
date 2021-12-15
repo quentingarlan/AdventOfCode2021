86cb286a7493991240494746a5d2e796e76f2bcc
Quentin.GARLAN <Quentin.GARLAN@solina-group.fr>
2021-12-15 09:49:42 +0100

    functional day 14 part 1

---
+++ Day14/test.spec.ts
@@ -0,0 +1,37 @@
+const assert = require('assert');
+const puzzleInput = require('./string.ts');
+let Advent1 = require('./Part1/adventPart1.ts');
+
+describe('Advent Calendar', function () {
+    describe('ExtendedPolymerization', function () {
+        it('should return 1588', function () {
+            const input: string = `NNCB
+
+            CH -> B
+            HH -> N
+            CB -> H
+            NH -> C
+            HB -> C
+            HC -> B
+            HN -> C
+            NN -> C
+            BH -> H
+            NC -> B
+            NB -> B
+            BN -> B
+            BB -> N
+            BC -> B
+            CC -> N
+            CN -> C`;
+            const result = Advent1.ExtendedPolymerization(input);
+            assert.equal(result, 1588);
+        }).timeout(50000);
+
+        it('should return 2233', function () {
+            const result = Advent1.ExtendedPolymerization(puzzleInput.inputTest);
+            assert.equal(result, 2233);
+        }).timeout(50000);
+
+    });
+
+});
